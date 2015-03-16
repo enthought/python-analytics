38e0fb90acca12d8d0083b9c2c99d250dc8345df
Simon Jagoe <simon@simonjagoe.com>
2015-03-16 14:02:25 +0000

    Add dev_requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,116 @@
+# (C) Copyright 2015 Enthought, Inc., Austin, TX
+# All right reserved.
+import os
+import subprocess
+
+from setuptools import setup, find_packages
+
+MAJOR = 0
+MINOR = 0
+MICRO = 1
+
+IS_RELEASED = False
+
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+
+# Return the git revision as a string
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, env=env,
+        ).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        git_revision = out.strip().decode('ascii')
+    except OSError:
+        git_revision = "Unknown"
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-list', '--count', 'HEAD'])
+        git_count = out.strip().decode('ascii')
+    except OSError:
+        git_count = '0'
+
+    return git_revision, git_count
+
+
+def write_version_py(filename='python_analytics/_version.py'):
+    template = """\
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '{version}'
+full_version = '{full_version}'
+git_revision = '{git_revision}'
+is_released = {is_released}
+
+if not is_released:
+    version = full_version
+"""
+    # Adding the git rev number needs to be done inside
+    # write_version_py(), otherwise the import of python_analytics._version
+    # messes up the build under Python 3.
+    fullversion = VERSION
+    if os.path.exists('.git'):
+        git_rev, dev_num = git_version()
+    elif os.path.exists('python_analytics/_version.py'):
+        # must be a source distribution, use existing version file
+        try:
+            from python_analytics._version import git_revision as git_rev
+            from python_analytics._version import full_version as full_v
+        except ImportError:
+            raise ImportError("Unable to import git_revision. Try removing "
+                              "python_analytics/_version.py and the build "
+                              "directory before building.")
+        import re
+        match = re.match(r'.*?\.dev(?P<dev_num>\d+)\+.*', full_v)
+        if match is None:
+            dev_num = '0'
+        else:
+            dev_num = match.group('dev_num')
+    else:
+        git_rev = "Unknown"
+        dev_num = '0'
+
+    if not IS_RELEASED:
+        fullversion += '.dev{0}+{1}'.format(dev_num, git_rev[:7])
+
+    with open(filename, "wt") as fp:
+        fp.write(template.format(version=VERSION,
+                                 full_version=fullversion,
+                                 git_revision=git_rev,
+                                 is_released=IS_RELEASED))
+
+
+if __name__ == "__main__":
+    write_version_py()
+    from python_analytics import __version__
+
+    setup(name="python-analytics",
+          version=__version__,
+          classifiers=[
+              'Development Status :: 1 - Planning',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+          ],
+          author="Enthought Inc.",
+          author_email="info@enthought.com",
+          packages=find_packages(),
+          install_requires=[
+              "requests >= 2.4.0, < 2.6.0, != 2.5.1",
+              "six",
+          ],
+          license="BSD")
