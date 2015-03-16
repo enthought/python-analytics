d8ca39fff983362a2d780234b5c91798f834f7d7
Simon Jagoe <simon@simonjagoe.com>
2015-03-16 14:56:43 +0000

    Add more tests

--- python_analytics/tests/test_tracker.py
+++ python_analytics/tests/test_tracker.py
@@ -119,3 +119,55 @@ class TestEvent(unittest.TestCase):
 
         # Then
         self.assertEqual(event_dict, expected)
+
+    def test_event_dimensions(self):
+        # Given
+        dimension_value = 'some-value'
+        dimension = CustomDimension(1, dimension_value)
+        category = 'category'
+        action = 'action'
+        label = 'Another event!'
+        value = 42
+        event = Event(
+            category=category, action=action, label=label, value=value,
+            custom_dimensions=[dimension])
+        expected = {
+            't': 'event',
+            'ec': category,
+            'ea': action,
+            'el': label,
+            'ev': value,
+            'cd1': dimension_value,
+        }
+
+        # When
+        event_dict = event.to_dict()
+
+        # Then
+        self.assertEqual(event_dict, expected)
+
+    def test_event_metrics(self):
+        # Given
+        metric_value = 28
+        metric = CustomMetric(1, metric_value)
+        category = 'category'
+        action = 'action'
+        label = 'Another event!'
+        value = 42
+        event = Event(
+            category=category, action=action, label=label, value=value,
+            custom_metrics=[metric])
+        expected = {
+            't': 'event',
+            'ec': category,
+            'ea': action,
+            'el': label,
+            'ev': value,
+            'cm1': metric_value,
+        }
+
+        # When
+        event_dict = event.to_dict()
+
+        # Then
+        self.assertEqual(event_dict, expected)
