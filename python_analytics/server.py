ed481918f0a84cf3f4f255204c7c4c02ae868711
Simon Jagoe <sjagoe@enthought.com>
2015-03-17 14:52:29 +0000

    Remove unnecessary code

--- python_analytics/server.py
+++ python_analytics/server.py
@@ -10,9 +10,3 @@ class ServerInfo(object):
 
     application_name = TrackedAttribute('an', text_type)
     response_time = TrackedAttribute('srt', int)
-
-    def __init__(self, application_name=None, response_time=None):
-        if application_name is not None:
-            self.application_name = application_name
-        if response_time is not None:
-            self.response_time = response_time
--- python_analytics/visitor.py
+++ python_analytics/visitor.py
@@ -10,9 +10,3 @@ class Visitor(object):
 
     user_agent = TrackedAttribute('ua', text_type)
     ip_address = TrackedAttribute('uip', text_type)
-
-    def __init__(self, user_agent=None, ip_address=None):
-        if user_agent is not None:
-            self.user_agent = user_agent
-        if ip_address is not None:
-            self.ip_address = ip_address
